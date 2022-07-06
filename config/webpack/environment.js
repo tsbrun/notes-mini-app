73338097b8e585d0a580fc3554c7ab35d086666c
github username <github email address>
2022-07-05 22:51:15 -0400

    Install clean-webpack-plugin to handle webpack error: Webpacker::Manifest::MissingEntryError

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// const webpackConfig = {
+//     plugins: [
+//         /**
+//          * All files inside webpack's output.path directory will be removed once, but the
+//          * directory itself will not be. If using webpack 4+'s default configuration,
+//          * everything under <PROJECT_DIR>/dist/ will be removed.
+//          * Use cleanOnceBeforeBuildPatterns to override this behavior.
+//          *
+//          * During rebuilds, all webpack assets that are not used anymore
+//          * will be removed automatically.
+//          *
+//          * See `Options and Defaults` for information
+//          */
+//         new CleanWebpackPlugin(),
+//     ],
+// };
+
+// module.exports = webpackConfig;
