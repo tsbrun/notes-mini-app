280d29f936d6d0d0a44523f7c3ee6b7e109d5bdb
github username <github email address>
2022-07-05 22:35:42 -0400

    Change column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_023047) do
+ActiveRecord::Schema.define(version: 2022_07_06_023451) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.string "desc"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
